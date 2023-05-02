4f815bfa8b3ec45f92d28493b02a5f5f7252c5f8
Qingying Meng <qmeng222@gmail.com>
2023-05-01 21:34:48 -0700

    implement server-client communication to pass data from server to front-end

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 import io from "socket.io-client";
 
 export class App extends Component {
+  constructor() {
+    super(); // set up inheritance
+    this.state = {
+      msg: "",
+    };
+  }
+
   componentDidMount() {
     const socket = io.connect("/");
+    socket.on("msg", (data) => this.setState({ msg: data }));
   }
 
   render() {
     return (
       <div>
-        <h1>This is client site.</h1>
+        <h1>{this.state.msg}</h1>
       </div>
     );
   }
